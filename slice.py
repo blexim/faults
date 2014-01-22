babec48fef78fa45642d467a47bfd76467c4408a
Matt Lewis <blexim@gmail.com>
2014-01-22 20:13:49 +0000

    Slicer/comparator

--- slice.py
+++ slice.py
@@ -5,9 +5,30 @@ import sys
 f1 = open(sys.argv[1])
 f2 = open(sys.argv[2])
 
+passed = open(sys.argv[3], 'w')
+failed = open(sys.argv[4], 'w')
+
+linelim = int(sys.argv[5])
+
+def add(l, f):
+  t = set(eval(l))
+
+  for i in xrange(linelim):
+    if (i+1) in t:
+      f.write("1 ")
+    else:
+      f.write("0 ")
+
+  f.write("\n")
+
 for (l1, l2) in zip(f1, f2):
   if l1 != l2:
-    print l1.strip()
+    add(l1, failed)
+  else:
+    add(l1, passed)
 
 f1.close()
 f2.close()
+
+passed.close()
+failed.close()
