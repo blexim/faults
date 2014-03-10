0bfc46763eb176350942b19433c0d5df7f8ab2d0
Matt <matt@cantab.net>
2014-03-10 13:57:02 +0000

    Evaluate all script

---
+++ evaluate_all.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+./evaluate.py outputs/tcas 173
+./evaluate.py outputs/schedule 410
+./evaluate.py outputs/schedule2 307
+./evaluate.py outputs/printtokens 563
+./evaluate.py outputs/printtokens2 508
+./evaluate.py outputs/totinfo 406
